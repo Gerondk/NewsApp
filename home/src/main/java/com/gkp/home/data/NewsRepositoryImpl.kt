3ad0e67b4023f29b836f12e0674bf1482e08d5b7
Gerond Kwenda <gerond@chargeAtFriends.com>
2024-08-05 17:21:57 +0200

    Fix Datetime parsing crash

--- home/src/main/java/com/gkp/home/data/NewsRepositoryImpl.kt
+++ home/src/main/java/com/gkp/home/data/NewsRepositoryImpl.kt
@@ -1,21 +1,24 @@
 package com.gkp.home.data
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.gkp.core.ResourceState
 import com.gkp.core.di.IoDispatcher
 import com.gkp.core.domain.NewsArticle
 import com.gkp.core.entities.AppConfig
 import com.gkp.core.safeApiCall
 import com.gkp.home.domain.NewsRepository
-import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
 
 class NewsRepositoryImpl @Inject constructor(
     private val newsApi: NewsApi,
     @IoDispatcher private val dispatcher: CoroutineDispatcher,
     private val appConfig: AppConfig
 ) : NewsRepository {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun getTopHeadlines(country: String): Flow<ResourceState<List<NewsArticle>>> =
         safeApiCall(
             mapper = { it.toArticlesList() },
